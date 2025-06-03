b5d562cc51603f16b4a63c1dc11fe7fdc45759f0
Parvej3D <parvejvertical@gmail.com>
2025-06-03 16:39:51 +0530

    Add files via upload

---
+++ types/model-viewer.d.ts
@@ -0,0 +1,24 @@
+// types/model-viewer.d.ts
+import React from 'react';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'model-viewer': React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      > & {
+        src?: string;
+        alt?: string;
+        'auto-rotate'?: boolean | string;
+        'camera-controls'?: boolean | string;
+        'ar'?: boolean | string;
+        'poster'?: string;
+        'shadow-intensity'?: number | string;
+        'exposure'?: number | string;
+        'camera-orbit'?: string;
+        style?: React.CSSProperties;
+      };
+    }
+  }
+}
