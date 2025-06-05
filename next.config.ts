c78829ec64f4f94ff2dab7c64f2a09fd50dc6d5c
Parvej Alam <parvejvertical@gmail.com>
2025-06-05 12:42:57 +0530

    Fix next.config.ts and move Tailwind theme to tailwind.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,29 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-module.exports = {
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Rubik', 'sans-serif'],
-      },
-    },
-  },
-
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   distDir: 'build',
-
+  reactStrictMode: true,
 };
-
-
 
 export default nextConfig;
+
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+// };
+
+// module.exports = {
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         sans: ['Rubik', 'sans-serif'],
+//       },
+//     },
+//   },
+
+//   distDir: 'build',
+
+// };
+
+// export default nextConfig;
 
